7c2fc855df0a7016096589e3df521fefa3b05d6d
91integ25 <91integ25@gmail.com>
2017-04-22 11:27:49 -0400

    routes and paths complete

--- routes.js
+++ routes.js
@@ -1,18 +1,34 @@
 var express = require("express");
+var path = require("path");
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = 3000
 var tablesArr = [];
 var waitList = [];
-app.get('/tables',function (req,res) {
-	res.send("this is tables");
+app.get('/all',function (req,res) {
+	res.sendFile(path.join(__dirname,"table.html"))
 });
 app.get('/reserve',function (req,res){
-	res.send("this is reserve");
+	res.sendFile(path.join(__dirname,"reservation.html"));
 });
 app.get('/api/:tables',function(req,res){
 	var newTable = req.params.tables;
-	console.log(req.params);
+	if(newTable === booked){
+		res.json(tablesArr);
+	}
+	else{
+		res.json(waitList);
+	}
 });
+
+app.post('/all', function(req,res){
+	if(tablesArr.length < 5){
+			tablesArr.push(req)
+	}
+	else{
+		waitList.push(req)
+	}
+})
 app.listen(PORT,function(){
 console.log("Listening on PORT: ",PORT);
 });

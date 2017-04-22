b36142546774fbe2240c67d0d2b7dab169d2f6e2
91integ25 <91integ25@gmail.com>
2017-04-22 11:06:05 -0400

    routes added

--- routes.js
+++ routes.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
 var PORT = 3000
-
+var tablesArr = [];
+var waitList = [];
 app.get('/tables',function (req,res) {
 	res.send("this is tables");
-})
+});
 app.get('/reserve',function (req,res){
 	res.send("this is reserve");
-})
+});
+app.get('/api/:tables',function(req,res){
+	var newTable = req.params.tables;
+	console.log(req.params);
+});
 app.listen(PORT,function(){
 console.log("Listening on PORT: ",PORT);
 });
